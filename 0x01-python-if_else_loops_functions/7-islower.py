90c946cad49cfaed61178b2289706c36ee55b669
Olabode Temitayo Elijah <Lilstylz4real@gmail.com>
2022-11-29 22:56:02 +0000

    loop

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - olabode temitayo elijah
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
